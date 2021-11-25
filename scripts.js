cef9564433741e87a83e58637d7bedf9700bdf03
TheZero0-ctrl <ankitpariyar486@gmail.com>
2021-11-25 18:00:18 +0545

    create ui version

--- scripts.js
+++ scripts.js
@@ -7,51 +7,68 @@ function computerPlay(){
 let playerScore = 0;
 let computerScore = 0;
 
-function playRound(computerSelection, playerSelection){
+function playRound(img){
+    const computerSelection = computerPlay();
+    const playerSelection = img.alt;
+    const remark = document.querySelector('.remark')
     if (computerSelection == playerSelection){
-        console.log(`compoter select ${computerSelection}`);
-        console.log("Draw");
+        remark.textContent = ".";
+        remark.textContent = "Draw";
     } else if (computerSelection == 'rock' && playerSelection == 'paper'){
-        console.log(`compoter select ${computerSelection}`);
-        console.log("You win");
+        remark.textContent = "You win";
         playerScore++;
     } else if (computerSelection == 'rock'  && playerSelection == 'scissor'){
-        console.log(`compoter select ${computerSelection}`);
-        console.log("You lose");
+        remark.textContent = "."
+        remark.textContent = "You loose";
         computerScore++;
     } else if (computerSelection == 'paper' && playerSelection == 'rock'){
-        console.log(`compoter select ${computerSelection}`);
-        console.log("you lose");
+        remark.textContent = ".";
+        remark.textContent = "You loose";
         computerScore++;
     } else if (computerSelection == 'paper' && playerSelection == 'scissor'){
-        console.log(`compoter select ${computerSelection}`);
-        console.log('You win');
+        remark.textContent = ".";
+        remark.textContent = "You win";
         playerScore++;
     } else if (computerSelection == "scissor" && playerSelection == 'paper'){
-        console.log(`compoter select ${computerSelection}`);
-        console.log("You lose");
+        remark.textContent = ".";
+        remark.textContent = "You loose";
         computerScore++;
     } else if (computerSelection == "scissor" && playerSelection == 'rock'){
-        console.log(`compoter select ${computerSelection}`);
-        console.log("You win");
+        remark.textContent = ".";
+        remark.textContent = "You win";
         playerScore++;
     }
 }
 
-
-
-
-for (let i = 0; i < 4; i++) {
-    const computerSelection = computerPlay()
-    const playerSelection = prompt("Your choice?").toLowerCase()
-    playRound(computerSelection, playerSelection)
-    console.log(`your score: ${playerScore}, computer score: ${computerScore}`)
-  } 
-
-if (computerScore < playerScore){
-    console.log("You did it, You won");
-} else if (computerScore > playerScore){
-    console.log("You fucking LOSSER");
-} else {
-    console.log("I expect more from you")
+const btns = document.querySelectorAll('.btn');
+console.log(btns);
+for (let i = 0; i< btns.length; i++){
+    btns[i].addEventListener("click", function game(){
+        const img = btns[i].querySelector("img");
+        playRound(img);
+        const yourResult = document.querySelector('.you');
+        const computerResult = document.querySelector(".computer");
+        yourResult.textContent = playerScore;
+        computerResult.textContent = computerScore;
+        if (playerScore == 4 || computerScore == 4){
+            winnerSelection(playerScore, computerScore);
+            playerScore = 0;
+            computerScore = 0;
+        }
+    })
 }
+
+
+    
+function winnerSelection(playerScore, computerScore) {
+    const remark = document.querySelector('.remark')
+    if (computerScore < playerScore){
+        remark.textContent = "You did it, You won"
+    } else if (computerScore > playerScore){
+        remark.textContent = "You fucking LOSSER"
+    } else {
+        remark.textContent = "I expect more from you"
+    }
+}
+
+
